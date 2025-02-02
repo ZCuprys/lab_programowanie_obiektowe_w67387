cce7adc3a7b9b081dfdd6191862d2e94e1fc399e
ZCuprys <166232500+ZCuprys@users.noreply.github.com>
2025-02-02 21:33:04 +0100

    Add files via upload

---
+++ LAB2/Zadanie4/Licz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie4
+{
+    public class Licz
+    {
+        private int Value;
+
+        public Licz(int value)
+        {
+            Value = value;
+        }
+        public void Dodaj(int liczba)
+        {
+            Value += liczba;
+        }
+        public void Odejmij(int liczba)
+        {
+            Value -= liczba;
+        }
+        public void WypiszStan()
+        {
+            Console.WriteLine("Wartość:" + Value);
+        }
+    }
+}
---
+++ LAB2/Zadanie4/Program.cs
@@ -0,0 +1,17 @@
+﻿using Zadanie4;
+
+Licz liczba1 = new Licz(0);
+Licz liczba2 = new Licz(0);
+
+liczba1.WypiszStan();
+liczba1.Dodaj(3);
+liczba1.WypiszStan();
+liczba1.Odejmij(1);
+liczba1.WypiszStan();
+
+liczba2.WypiszStan();
+liczba2.Dodaj(3);
+liczba2.WypiszStan();
+liczba2.Odejmij(1);
+liczba2.WypiszStan();
+
