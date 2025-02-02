cd7fef13a71dea550295ce92e5d16186cc50856e
ZCuprys <166232500+ZCuprys@users.noreply.github.com>
2025-02-02 21:32:03 +0100

    Add files via upload

---
+++ LAB2/Zadanie2/KontoBankowe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    internal class KontoBankowe
+    {
+        // Właściwości
+        public string Wlasciciel;
+        public decimal Saldo;
+
+        // Konstruktor
+        public KontoBankowe(string wlasciciel, decimal poczatkoweSaldo)
+        {
+            if (string.IsNullOrWhiteSpace(wlasciciel))
+            {
+                Console.WriteLine("Właściciel nie może być pusty.");
+            }
+            Wlasciciel = wlasciciel;
+            Saldo = poczatkoweSaldo;
+        }
+        // Metoda do wpłaty środków
+        public void Wplata(decimal kwota)
+        {
+            if (kwota <= 0)
+            {
+                Console.WriteLine("Kwota musi być dodatnia");
+            }   
+            if (kwota >= 0)
+            {
+                Saldo += kwota;
+            } 
+        }
+        //Metoda do wypłaty środków
+        public void Wyplata(decimal kwota)
+        {
+            if (kwota <= 0)
+            {
+                Console.WriteLine("Kwota wypłaty musi być dodatnia.");
+            }
+            
+            if (kwota > Saldo)
+            {
+                Console.WriteLine("Niewystarczające środki na koncie.");
+            }
+            Saldo -= kwota;
+        }
+    }
+}
---
+++ LAB2/Zadanie2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Zadanie2;
+
+//Zadanie 2
+KontoBankowe konto = new KontoBankowe("Jan Kowalski", 1000);
+konto.Wplata(500);
+konto.Wyplata(200);
+Console.WriteLine($"Saldo: {konto.Saldo}");
